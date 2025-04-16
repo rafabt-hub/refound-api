a84b86689a49321c02c305ab6050c81707b4c4ee
Rafael <rafaelbthomaz@gmail.com>
2025-04-16 16:28:47 -0300

    adding authentications

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,8 @@ import { usersRoutes } from "./users-routes"
 import { sessionsRoutes } from "./sessions-routes"
 import { refundsRoutes } from "./refunds-routes"
 
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
+
 const routes = Router()
 
 // Rotas publicas.
@@ -11,6 +13,7 @@ routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
 
 // Rotas privadas.
+routes.use(ensureAuthenticated)
 routes.use("/refunds", refundsRoutes)
 
 export { routes }
