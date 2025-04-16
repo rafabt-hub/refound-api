6938721ed5d7ebee8a3cb1ad022f980ab8e90a8b
Rafael <rafaelbthomaz@gmail.com>
2025-04-16 15:56:41 -0300

    sessions controller config

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,31 @@
 import { Request, Response } from "express"
+import { prisma } from "@/database/prisma"
+import { z } from "zod"
+import { AppError } from "@/utils/AppError"
+import { compare } from "bcrypt"
 
 class SessionsController {
   async create(request: Request, response: Response) {
-    response.json({ message: "ok" })
+    const bodySchema = z.object({
+      email: z.string().email({ message: "E-mail invalido."}),
+      password: z.string(),
+    })
+
+    const { email, password } = bodySchema.parse(request.body)
+
+    const user = await prisma.user.findFirst({ where: { email }})
+
+    if(!user){
+      throw new AppError("E-mail ou senha invalidos.")
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if(!passwordMatched) {
+      throw new AppError("E-mail ou senha invalidos", 401)
+    }
+
+    response.json({ email, password })
   }
 }
 
