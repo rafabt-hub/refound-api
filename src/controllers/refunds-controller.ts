9ffb69dc357bddbcecbbe998e0b85d1f9858191c
Rafael <rafaelbthomaz@gmail.com>
2025-04-18 10:17:06 -0300

    adding refund controller

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,8 +1,42 @@
 import { Request, Response } from "express"
+import { prisma } from "@/database/prisma"
+import { AppError } from "@/utils/AppError"
+import { z } from "zod"
+
+const CategoriesEnum = z.enum([
+  "food",
+  "others",
+  "services",
+  "transport",
+  "accomodation"
+])
 
 class RefundsController {
   async create(request: Request, response: Response){
-    response.json({ message: "ok "})
+    const bodySchema = z.object({
+      name: z.string().trim().min(1, { message: "Informe o nome da solicitação."}),
+      category: CategoriesEnum,
+      amount: z.number().positive({ message: "O valor precisa ser positivo."}),
+      filename: z.string().min(20),
+    })
+
+    const { name, category, amount, filename } = bodySchema.parse(request.body)
+
+    if(!request.user?.id){
+      throw new AppError("Unauthorized", 401)
+    }
+
+    const refund = await prisma.refunds.create({
+      data: {
+        name,
+        category,
+        amount,
+        filename,
+        userId: request.user.id,
+      },
+    })
+
+    response.status(201).json(refund)
   }
 }
 
