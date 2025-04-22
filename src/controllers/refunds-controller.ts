43d8ea606cb6bc20a11c0caeda8a945e61d159b2
Rafael <rafaelbthomaz@gmail.com>
2025-04-22 15:08:16 -0300

    adding show routes

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -40,7 +40,67 @@ class RefundsController {
   }
 
   async index(request: Request, response: Response) {
-    response.json({ message: "ok"})
+    const querySchema = z.object({
+      name: z.string().optional().default(""),
+      page: z.coerce.number().optional().default(1),
+      perPage: z.coerce.number().optional().default(10),
+    })
+
+    const { name, page, perPage } = querySchema.parse(request.query)
+
+    const skip = (page - 1) * perPage
+
+    const refunds = await prisma.refunds.findMany({
+      skip,
+      take: perPage,
+      where: {
+        user: {
+          name: {
+            contains: name.trim(),
+          },
+        },
+      },
+      orderBy: { createdAt: "desc" },
+      include: { user: true },
+    })
+
+    const totalRecords = await prisma.refunds.count({
+      where: {
+        user: {
+          name: {
+            contains: name.trim(),
+          },
+        },
+      },
+    })
+
+    const totalPages = Math.ceil(totalRecords / perPage)
+
+
+    response.json({
+      refunds,
+      pagination: {
+        page,
+        perPage,
+        totalRecords,
+        totalPages: totalPages > 0 ? totalPages : 1,
+      },
+    })
+  }
+
+  async show(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const refund = await prisma.refunds.findFirst({
+      where: { id },
+      include: { user: true },
+    })
+
+    response.json(refund)
   }
 }
 
--- src/routes/refunds-routes.ts
+++ src/routes/refunds-routes.ts
@@ -18,5 +18,11 @@ refundsRoutes.post(
     refundsController.index
   )
 
+  refundsRoutes.get(
+    "/:id", 
+    verifyUserAuthorization(["manager", "manager"]),
+    refundsController.show
+  )
+
 export { refundsRoutes }
 
